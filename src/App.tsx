f45679bc021d7b0d6e6ff597e58a8b410ea1a8ef
Didin Yahya <didinnuryahya@gmail.com>
2023-12-05 13:53:16 +0700

    add react helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,54 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { Footer } from './components/Footer/Footer'
-import { Header } from './components/Header/Header'
-import { Main } from './components/Main/Main'
-import { GlobalStyle } from './styles/global'
-import 'react-toastify/dist/ReactToastify.css'
+import React from "react";
+import { Helmet } from "react-helmet";
+import { useEffect } from "react";
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+import { Main } from "./components/Main/Main";
+import { GlobalStyle } from "./styles/global";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   return (
     <>
-      <GlobalStyle></GlobalStyle>
-      <Header></Header>
-      <Main></Main>
-      <Footer></Footer>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Didin Nur Yahya</title>
+        <meta
+          name="description"
+          content="Didin Nur Yahya, A developer and a Freelancer. I create interactive & responsive Websites, Mobile Apps, and Test automation projects. Experience with E-Commerce site development."
+        />
+        <meta
+          name="keywords"
+          content="Didin Nur Yahya, Didin, Didin nur yahya LKS"
+        />
+
+        {/* Open Graph tags */}
+        <meta property="og:title" content="Didin Nur Yahya" />
+        <meta
+          property="og:description"
+          content="Didin Nur Yahya, A developer and a Freelancer. I create interactive & responsive Websites, Mobile Apps, and Test automation projects. Experience with E-Commerce site development."
+        />
+        <meta property="og:image" content="/Images/.png" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image" content="/Images/favicon.png" />
+        <meta name="image" property="og:image" content="/Images/favicon.png" />
+        <meta property="og:image:type" content="image/jpg" />
+        <meta property="og:image:width" content="800" />
+        <meta property="og:image:height" content="600" />
+        <meta property="og:type" content="website" />
+        <meta property="og:type" content="article" />
+        <meta property="article:author" content="http://github.com/dzoxploit" />
+        <meta property="og:site_name" content="didinnuryahya.vercell.app" />
+        <meta property="article:published_time" content="date_time" />
+        <meta property="og:locale" content="pt_BR" />
+      </Helmet>
+
+      <GlobalStyle />
+      <Header />
+      <Main />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
