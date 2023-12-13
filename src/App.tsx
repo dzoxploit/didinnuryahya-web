8e765774e5f6bfc214faf5495a8d824a588a7c92
Didin Yahya <didinnuryahya@gmail.com>
2023-12-13 10:18:14 +0700

    perbaikan react head

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HeadProvider } from "react-head";
+import { HeadProvider, Title, Link, Meta } from "react-head";
 import { GlobalStyle } from "./styles/global";
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
@@ -12,53 +12,52 @@ function App() {
     <>
       <HeadProvider>
         {/* Facebook Meta Tags */}
-        <meta property="og:title" content="DNY Aka Didin Nur Yahya" />
-        <meta
+        <Meta property="og:title" content="DNY Aka Didin Nur Yahya" />
+        <Meta
           property="og:description"
           content="Didin Nur Yahya, Software Engineer and Test Engineer"
         />
-        <meta property="og:url" content="https://didinnuryahya.vercel.app" />
+        <Meta property="og:url" content="https://didinnuryahya.vercel.app" />
         <meta property="og:type" content="website" />
 
-        <meta
+        <Meta
           property="og:image"
           content={"https://didinnuryahya.vercel.app" + logoIcon}
         />
-        <meta property="og:image:type" content="image/png" />
+        <Meta property="og:image:type" content="image/png" />
 
         {/* Google / Search Engine Tags */}
-        <meta itemProp="name" content="Didin Nur Yahya" />
-        <meta
+        <Meta itemProp="name" content="Didin Nur Yahya" />
+        <Meta
           itemProp="description"
           content="Didin Nur Yahya, Software Engineer and Test Engineer"
         />
-        <meta
+        <Meta
           itemProp="image"
           content={"https://didinnuryahya.vercel.app" + logoIcon}
         />
 
         {/* Twitter Meta Tags */}
-        <meta
+        <Meta
           name="twitter:card"
           content={"https://didinnuryahya.vercel.app" + logoIcon}
         />
-        <meta name="twitter:title" content="Didin Nur Yahya" />
-        <meta
+        <Meta name="twitter:title" content="Didin Nur Yahya" />
+        <Meta
           name="twitter:description"
           content="Didin Nur Yahya, Software Engineer and Test Engineer"
         />
-        <meta
+        <Meta
           name="twitter:image"
           content={"https://didinnuryahya.vercel.app" + logoIcon}
         />
         {/* Facebook App ID */}
-        <meta property="fb:app_id" content="1166728617405476" />
-
-        <GlobalStyle />
-        <Header />
-        <Main />
-        <Footer />
+        <Meta property="fb:app_id" content="1166728617405476" />
       </HeadProvider>
+      <GlobalStyle />
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
