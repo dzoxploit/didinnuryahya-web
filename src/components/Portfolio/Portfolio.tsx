d5cf3f20201c8e57c03c099106088a7d7c55e923
Didin Yahya <didinnuryahya@gmail.com>
2024-08-01 14:49:56 +0700

    Update Portofolio

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -28,6 +28,51 @@ export function Portfolio() {
               </svg>
               <div className="project-links">
                 <a
+                  href="http://pass-logistic.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={githubIcon} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Web Pass-logistic.com</h3>
+              <p>
+                website company profile to get information about history of
+                company and courier price based on route and also optimization
+                SEO
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Lumen</li>
+                <li>MYSQL</li>
+                <li>Next JS</li>
+                <li>React JS</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#23ce6b "
+                stroke-width="1"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                <a
                   href="https://github.com/dzoxploit/api-moneyqu-laravel"
                   target="_blank"
                   rel="noreferrer"
