dce93a3cad7ba69b6f41eae68d1afa96acf4e221
Didin Yahya <didinnuryahya@gmail.com>
2024-08-02 10:17:35 +0700

    remove form

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,7 +1,6 @@
 import { Container } from "./styles";
 import emailIcon from "../../assets/email-icon.svg";
 import phoneIcon from "../../assets/phone-icon.svg";
-import { Form } from "../Form/Form";
 
 export function Contact() {
   return (
@@ -23,7 +22,6 @@ export function Contact() {
           <a href="tel:+6281413309326">(+62) 81413309326</a>
         </div>
       </div>
-      <Form></Form>
     </Container>
   );
 }
